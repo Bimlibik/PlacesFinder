1dc568ca7d731082f72988b239fd02344f32d1fd
Factis <53155715qwerty>
2020-10-12 18:59:10 +0800

    optimized imports

--- app/src/main/java/com/foxy/testproject/mvp/MapView.kt
+++ app/src/main/java/com/foxy/testproject/mvp/MapView.kt
@@ -2,11 +2,7 @@ package com.foxy.testproject.mvp
 
 import com.here.android.mpa.common.GeoCoordinate
 import com.here.android.mpa.mapping.Map
-import com.here.android.mpa.mapping.MapMarker
-import com.here.android.mpa.mapping.MapObject
-import com.here.android.mpa.search.DiscoveryResult
 import com.here.android.mpa.search.ErrorCode
-import com.here.android.mpa.search.PlaceLink
 import moxy.MvpView
 import moxy.viewstate.strategy.AddToEndSingleStrategy
 import moxy.viewstate.strategy.StateStrategyType
